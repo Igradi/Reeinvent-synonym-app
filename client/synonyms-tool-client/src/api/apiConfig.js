9961f7dbeb849fc9608b91fbf1d6c42f67ef81ea
Ivor Gradinski <ivorgradinski@gmail.com>
2024-09-26 01:54:54 +0200

    First commit.

---
+++ client/synonyms-tool-client/src/api/apiConfig.js
@@ -0,0 +1,2 @@
+export const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
+export const SYNONYM_API = `${BASE_URL}/api/synonyms`;
---
+++ server/validators/synonymValidator.js
@@ -0,0 +1,15 @@
+const Joi = require('joi');
+
+const addSynonymSchema = Joi.object({
+    word: Joi.string().min(1).required(),
+    synonyms: Joi.array().items(Joi.string().min(1)).min(1).required()
+});
+
+const getSynonymSchema = Joi.object({
+    word: Joi.string().min(1).required()
+});
+
+module.exports = {
+    addSynonymSchema,
+    getSynonymSchema
+};
